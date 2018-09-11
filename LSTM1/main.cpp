e14ab923f0e4cc2f58dded809dfd0cd45c4e46dc
Angle Qian <angleqian01@gmail.com>
2018-09-11 09:15:00 +0800

    Initial Commit

---
+++ LSTM1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LSTM1
+//
+//  Created by Angle Qian on 11/09/2018.
+//  Copyright © 2018 Angle Qian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
